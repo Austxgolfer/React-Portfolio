4e86691b28547dd73744ed11412491eb56a167b4
MAX <47061140+Austxgolfer@users.noreply.github.com>
2019-07-04 21:21:13 -0500

    added contact form and split project cards into a seperate component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MainPage from "./components/Mainpg/main";
 import ProjectPage from "./components/Projpg/proj";
+import ContactForm from "./components/Contactpg/form";
 import "./App.css";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
     <div>
       <MainPage />
       <ProjectPage />
+      <ContactForm />
     </div>
   );
 }
--- src/components/Projpg/proj.js
+++ src/components/Projpg/proj.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ProjectCard from "./projcard";
 
 export default function ProjectPage() {
   return (
@@ -23,66 +24,7 @@ export default function ProjectPage() {
         </a>
       </nav>
       <div className="row">
-        <div className="col-md-3 center-block">
-          <div className="card mx-auto">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3 center-block">
-          <div className="card mx-auto">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3 center-block">
-          <div className="card mx-auto">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3 center-block">
-          <div className="card mx-auto">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
+        <ProjectCard />
       </div>
     </div>
   );
